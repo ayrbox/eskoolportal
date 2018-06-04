10a5650c6c7ede3311b4ec14633b28135eb59ed7
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-06-04 20:41:56 +0100

    Add: validation for user login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,10 @@ const bcrypt = require("bcryptjs");
 const token = require("jsonwebtoken");
 const passport = require("passport");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 const secretKey = require("../../config/keys").secretKey;
 
 //Load user model
@@ -14,9 +18,17 @@ const User = require("../../models/User");
 // @desc  Register user
 // @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
@@ -54,10 +66,17 @@ router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email }).then(user => {
     //Check user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found.";
+      return res.status(404).json(errors);
     }
 
     const payload = {
@@ -76,7 +95,8 @@ router.post("/login", (req, res) => {
           });
         });
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });

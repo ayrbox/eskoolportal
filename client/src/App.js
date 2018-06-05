23ca74329b22f02c17da97fe95973f76ce7a96c5
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-06-05 23:37:08 +0100

    UPDATE: Login and register screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>
-          <i class="fab fa-react" /> React Application
-        </h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+
+          <Route exact path="/" component={Landing} />
+
+          <div className="contianer">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }

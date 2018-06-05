f27de8124ab54052d78ce61833c1fd645eabad75
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-06-05 22:33:48 +0100

    ADD: React for frontend

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          <i class="fab fa-react" /> React Application
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;

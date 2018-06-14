35ce4cf88c7e64dc13fa307591a504f52ecd1862
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-06-14 17:45:59 +0100

    Fix: serve static assests

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use("/api/students", students);
 //serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   //set Static Folder
-  app.user(express.static("client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });

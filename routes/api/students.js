d7c76388899e31aa4b4d14a246d7e5765b4dad84
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-06-19 18:28:29 +0100

    Add: Student api all/get/post

--- routes/api/students.js
+++ routes/api/students.js
@@ -1,16 +1,75 @@
 const express = require("express");
+const Student = require("../../models/Student");
+const passport = require("passport");
 
 const router = express.Router();
 
-// @route GET api/students/test
-// @desc Test of student
-// @access Public
-router.get("/test", (req, res) => res.json({ message: "Students works" }));
+// @route GET api/students/
+// @desc list all the students
+// @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    let errors = {};
+    Student.find()
+      .then(students => {
+        if (!students) {
+          errors.students = "There are no studnet records";
+          return res.status(404).json(errors);
+        }
+        res.json(students);
+      })
+      .catch(err => {
+        res.status(500).json({ studnets: "Error getting students" });
+      });
+  }
+);
 
+// @route GET /api/students/:id
+// @desc get student profile
+// @desc Private
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    let errors = {};
+    Student.findOne()
+      .then(student => {
+        if (!student) {
+          errors.student = "Student not found";
+          return res.status(404).json(errors);
+        }
+        res.json(student);
+      })
+      .catch(err => {
+        res.status(500).json({ student: "Error getting student" });
+      });
+  }
+);
 
+// @route POST /api/students
+// @desc create new student profile
+// @desc Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    console.log(req.body);
+    //const { errros, isValid } = validateStudentInput(req.body);
 
-router.get('/students', (req, res) => {
-    res.json({'students': 'listo fo students'});
-})
+    const studentProfile = {};
+    studentProfile.name = req.body.name;
+    studentProfile.dateOfBirth = req.body.dateOfBirth;
+    studentProfile.gender = req.body.gender;
+    studentProfile.address = req.body.address;
+    studentProfile.contactNo = req.body.contactNo;
+    studentProfile.email = req.body.email;
+
+    console.log(studentProfile);
+
+    new Student(studentProfile).save().then(studnet => res.json(student));
+  }
+);
 
 module.exports = router;

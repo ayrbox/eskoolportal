feba78f7367eac898f00a3f59ceffbc21e71142d
ayrbox <srdx7@hotmail.com>
2020-05-17 00:32:44 +0100

    Model index file to export model

--- packages/api/models/index.js
+++ packages/api/models/index.js
@@ -4,47 +4,53 @@
  *
  */
 
-const UserModel = require('./User');
+require('dotenv').config();
 
+const Sequelize = require('sequelize');
+const UserModel = require('./User');
 const StudentModel = require('./Student');
 const ClassModel = require('./Class');
 const SectionModel = require('./Section');
 
-module.exports = function (connection) {
-  const User = UserModel(connection);
+const { DB_HOST, DB, DB_USER, DB_PASSWORD, DB_PORT } = process.env;
 
-  const Student = StudentModel(connection);
-  const Class = ClassModel(connection);
-  const Section = SectionModel(connection);
+const connection = new Sequelize(DB, DB_USER, DB_PASSWORD, {
+  dialect: 'postgres',
+  host: DB_HOST,
+  port: DB_PORT,
+  logging: false,
+});
 
-  // Map assications here
+const User = UserModel(connection);
+const Student = StudentModel(connection);
+const Class = ClassModel(connection);
+const Section = SectionModel(connection);
 
-  Student.belongsTo(Class, {
-    foreignKey: { name: 'class_id', allowNull: false },
-  });
-  Class.hasMany(Student, {
-    foreignKey: {
-      name: 'class_id',
-      allowNull: false,
-    },
-  });
-  Student.belongsTo(Section, {
-    foreignKey: {
-      name: 'section_id',
-      allowNull: false,
-    },
-  });
-  Section.hasMany(Student, {
-    foreignKey: {
-      name: 'section_id',
-      allowNull: false,
-    },
-  });
+Student.belongsTo(Class, {
+  foreignKey: { name: 'class_id', allowNull: false },
+});
+Class.hasMany(Student, {
+  foreignKey: {
+    name: 'class_id',
+    allowNull: false,
+  },
+});
+Student.belongsTo(Section, {
+  foreignKey: {
+    name: 'section_id',
+    allowNull: false,
+  },
+});
+Section.hasMany(Student, {
+  foreignKey: {
+    name: 'section_id',
+    allowNull: false,
+  },
+});
 
-  return {
-    User,
-    Student,
-    Class,
-    Section,
-  };
+module.exports = {
+  User,
+  Student,
+  Class,
+  Section,
 };

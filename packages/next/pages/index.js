ee5f189a5b214ac0de4f7ca73c6343ed880d2cfc
ayrbox <srdx7@hotmail.com>
2020-05-19 02:24:19 +0100

    Update student list layout

--- packages/next/pages/index.js
+++ packages/next/pages/index.js
@@ -5,16 +5,162 @@ import Layout from '../components/Layout';
 const Index = ({ students }) => {
   return (
     <Layout>
-      <h1>Get all students</h1>
-      <ol>
-        {students.map(({ id, name }) => (
-          <li key={id}>
-            <Link href={`/student/${id}`}>
-              <a>{name}</a>
-            </Link>
-          </li>
-        ))}
-      </ol>
+      <h1>Students</h1>
+      <div className="ibox ">
+        <div className="ibox-title">
+          <h5>Custom responsive table </h5>
+          <div className="ibox-tools">
+            <a className="collapse-link">
+              <i className="fa fa-chevron-up"></i>
+            </a>
+            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
+              <i className="fa fa-wrench"></i>
+            </a>
+            <ul className="dropdown-menu dropdown-user">
+              <li>
+                <a href="#" className="dropdown-item">
+                  Config option 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="dropdown-item">
+                  Config option 2
+                </a>
+              </li>
+            </ul>
+            <a className="close-link">
+              <i className="fa fa-times"></i>
+            </a>
+          </div>
+        </div>
+        <div className="ibox-content">
+          <div className="row">
+            <div className="col-sm-5 m-b-xs">
+              <select className="form-control-sm form-control input-s-sm inline">
+                <option value="0">Class: 1</option>
+                <option value="0">Class: 2</option>
+                <option value="0">Class: 3</option>
+                <option value="0">Class: 4</option>
+                <option value="0">Class: 5</option>
+              </select>
+            </div>
+            <div className="col-sm-4 m-b-xs">
+              <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                <label className="btn btn-sm btn-white active">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option1"
+                    autocomplete="off"
+                    checked
+                  />{' '}
+                  All
+                </label>
+                <label className="btn btn-sm btn-white">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option2"
+                    autocomplete="off"
+                  />{' '}
+                  A
+                </label>
+                <label className="btn btn-sm btn-white">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option3"
+                    autocomplete="off"
+                  />{' '}
+                  B
+                </label>
+                <label className="btn btn-sm btn-white">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option4"
+                    autocomplete="off"
+                  />{' '}
+                  C
+                </label>
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div className="input-group">
+                <input
+                  placeholder="Search"
+                  type="text"
+                  className="form-control form-control-sm"
+                />
+                <span className="input-group-append">
+                  {' '}
+                  <button type="button" className="btn btn-sm btn-primary">
+                    Go!
+                  </button>{' '}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="table-responsive">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>DOB</th>
+                  <th>Gender</th>
+                  <th>Address</th>
+                  <th>Contact no</th>
+                  <th>Email</th>
+
+                  <th>Join Date</th>
+                  <th>Class</th>
+                  <th>Section</th>
+                </tr>
+              </thead>
+              <tbody>
+                {students.map(
+                  ({
+                    id,
+                    name,
+                    dateOfBirth,
+                    gender,
+                    address,
+                    contactNo,
+                    email,
+                    joinDate,
+                    class: studentClass,
+                    section,
+                  }) => (
+                    <tr key={id}>
+                      <td>
+                        <Link href={`/student/${id}`}>
+                          <a>{name}</a>
+                        </Link>
+                      </td>
+                      <td>{dateOfBirth}</td>
+                      <td>{gender}</td>
+                      <td>{address}</td>
+                      <td>{contactNo}</td>
+                      <td>{email}</td>
+                      <td>{joinDate}</td>
+                      <td>
+                        <Link href={`/class/${studentClass.id}`}>
+                          <a>{studentClass.name}</a>
+                        </Link>
+                      </td>
+                      <td>
+                        <Link href={`/class/${section.id}`}>
+                          <a>{section.name}</a>
+                        </Link>
+                      </td>
+                    </tr>
+                  ),
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };

17a9e27efc61d44f2fd8973b0fec8ca6f7109a2f
ayrbox <srdx7@hotmail.com>
2020-05-31 00:58:29 +0100

    Configure User Session with express session and passport.

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -3,9 +3,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const { Connection, User } = require("./models");
+
+const session = require("./middlewares/session");
+const passportMiddleware = require("./middlewares/passport");
+
 const routes = require("./routes");
 
-app.use(bodyParser());
+const passport = passportMiddleware(User);
+
+app.use(bodyParser.json());
+app.use(session(Connection));
+
+// app.use(passportInit());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
 module.exports = app;
---
+++ packages/api/src/middlewares/session.js
@@ -0,0 +1,28 @@
+const session = require("express-session");
+const SequelizeSessionConnector = require("connect-session-sequelize");
+
+const SequelizeSessionStore = SequelizeSessionConnector(session.Store);
+
+const sessionMiddleware = function (sequelizeConnection, sync = false) {
+  const sessionStore = new SequelizeSessionStore({
+    db: sequelizeConnection
+  });
+
+  if (sync) {
+    sessionStore.sync();
+  }
+
+  return session({
+    secret: "d}*kKiVJo$wkQAD7wUpzqe3HKqsK)bPjynjDsvT6Lar3Fk>LifCKPdogLPxgDHud", //enter a random string here
+    resave: false,
+    saveUninitialized: true,
+    name: "eskoolportal",
+    cookie: {
+      secure: false, //CRITICAL on localhost
+      maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
+    },
+    store: sessionStore
+  });
+};
+
+module.exports = sessionMiddleware;

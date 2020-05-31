e2221996da5c2355461788420a0bcfd8750b854f
ayrbox <srdx7@hotmail.com>
2020-05-31 02:38:17 +0100

    Secure routes depending on secure attribute on route definition.

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -3,13 +3,18 @@ const { Router } = require("express");
 const studentRoutes = require("./students");
 const classRoutes = require("./class");
 const authRoutes = require("./auth");
+const secureRoute = require("../middlewares/secureRoute");
 
 const expressRouter = Router();
 
 const routes = [...studentRoutes, ...classRoutes, ...authRoutes];
 
-routes.forEach(({ method, path, handler }) => {
-  expressRouter[method](path, handler);
+routes.forEach(({ method, path, handler, secure }) => {
+  if (secure) {
+    expressRouter[method](path, secureRoute, handler);
+  } else {
+    expressRouter[method](path, handler);
+  }
 });
 
 module.exports = expressRouter;

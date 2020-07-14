e9d03f20ab6a08ba6916d191a22337ef3434983a
ayrbox <srdx7@hotmail.com>
2020-07-14 21:44:27 +0100

    Fix browser back button error req not exits.

--- packages/next/pages/_app.js
+++ packages/next/pages/_app.js
@@ -23,26 +23,32 @@ const getIntitalProps = async appContext => {};
 Main.getInitialProps = async appContext => {
   const { ctx } = appContext;
   const { req, res } = ctx;
-  console.log(ctx);
 
-  appContext.ctx.req.baseUrl = getBaseUrl(req);
+  if (req) {
+    appContext.ctx.req.baseUrl = getBaseUrl(req);
 
-  // User exists
-  if (req && req.session && req.session.passport && req.session.passport.user) {
-    return await App.getInitialProps(appContext); // perhaps required
+    // User exists
+    if (
+      req &&
+      req.session &&
+      req.session.passport &&
+      req.session.passport.user
+    ) {
+      return await App.getInitialProps(appContext); // perhaps required
+    }
+
+    // Do not redirect if excluded
+    if (isSecurityExcluded(req)) {
+      return await App.getInitialProps(appContext);
+    }
+
+    // redirect
+    res.writeHead(302, {
+      Location: '/login?redirected=true',
+    });
+    res.end();
   }
-
-  // Do not redirect if excluded
-  if (isSecurityExcluded(req)) {
-    return await App.getInitialProps(appContext);
-  }
-
-  // redirect
-  res.writeHead(302, {
-    Location: '/login?redirected=true',
-  });
-  res.end();
-  return;
+  return await App.getInitialProps(appContext);
 };
 
 export default Main;

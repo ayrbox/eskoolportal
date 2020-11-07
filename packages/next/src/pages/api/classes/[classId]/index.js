d024695606da1eb6ea5765ab2635b9bbaab2593d
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2020-11-07 04:21:53 +0000

    Add sesssion.

--- packages/next/src/pages/api/classes/[classId]/index.js
+++ packages/next/src/pages/api/classes/[classId]/index.js
@@ -1,8 +1,13 @@
-const { Class } = require("@eskoolportal/api/src/models");
+import nextConnect from "next-connect";
+import models from "@eskoolportal/api/src/models";
 
-export default async function handler(req, res) {
+import auth from "../../../../middlewares/auth";
+
+const handler = async function (req, res) {
   const { classId } = req.query;
-  const classInfo = await Class.findByPk(classId);
+  const classInfo = await models.Class.findByPk(classId);
 
   res.status(200).json(classInfo);
-}
+};
+
+export default nextConnect().use(auth).get(handler);

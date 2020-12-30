7b83bcf0e8c3769f547ade7a7a7bbd4c694dfd6e
ayrbox <sabin.dangol@hotmail.com>
2020-12-30 03:20:30 +0000

    API module based in express.

--- packages/api/src/handlers/class/makeClassListHandler.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ Class }) => {
-  return function(_, res) {
-    Class.findAll({
-      order: [["order", "ASC"]]
-    }).then(classes => {
-      res.json(classes);
-    });
-  };
-};

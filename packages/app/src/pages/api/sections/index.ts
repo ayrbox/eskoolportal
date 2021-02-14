dd0de5fb80432244a180850d1b00eee846891a3f
ayrbox <sabin.dangol@hotmail.com>
2021-02-14 19:08:07 +0000

    Update layout.

--- packages/app/src/pages/api/sections/index.ts
+++ packages/app/src/pages/api/sections/index.ts
@@ -1,7 +1,6 @@
 import { Section } from '@eskoolportal/core/lib/entities/Section';
-import { withAuthentication } from '~/lib/secureRoute';
-
-export default withAuthentication(async function handler(req, res) {
+import { secureRoute } from '~/lib/secureRoute';
+export default secureRoute(async function handler(req, res) {
   const sections = await Section.find();
 
   res.status(200).json(sections);

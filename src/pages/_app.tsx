c38ba846884f8b5edc3cc2b918bc021fe10c1874
ayrbox <sabin.dangol@hotmail.com>
2021-02-14 20:34:07 +0000

    Add nprogress.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,31 @@
-// import App from 'next/app';
+import { Provider } from 'next-auth/client';
+import { AppProps } from 'next/app';
+import { ReactElement, useState } from 'react';
+import Router from 'next/router';
 
 import '@styles/main.scss';
+import 'nprogress/nprogress.css';
 
-function Application(props) {
-  if (!props) {
-    return () => null;
-  }
+import NProgress from 'nprogress';
 
-  const { Component, pageProps } = props;
-  return <Component {...pageProps} />;
+function Application({ Component, pageProps }: AppProps): ReactElement {
+  const handleLoading = (state: boolean) => () => {
+    if (state) {
+      NProgress.start();
+    } else {
+      NProgress.done();
+    }
+  };
+
+  Router.events.on('routeChangeStart', handleLoading(true));
+  Router.events.on('routeChangeComplete', handleLoading(false));
+  Router.events.on('routeChangeError', handleLoading(false));
+
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default Application;

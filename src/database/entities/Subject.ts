7ddd0a72c6b753fa8ac051d1ecef91decb3b89ea
ayrbox <sabin.dangol@hotmail.com>
2021-03-28 22:52:25 +0100

    Add migration for class subjects.

---
+++ src/database/entities/Subject.ts
@@ -0,0 +1,31 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Class } from './Class';
+
+@Entity('subjects')
+export class Subject extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 200 })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToMany(() => Class, (_) => _.subjects)
+  classes: Class[];
+}
--- src/database/entities/index.ts
+++ src/database/entities/index.ts
@@ -1,6 +1,7 @@
 import { Class } from './Class';
 import { Section } from './Section';
 import { Student } from './Student';
+import { Subject } from './Subject';
 import { User } from './User';
 
-export default [Class, Section, Student, User];
+export default [Class, Section, Student, User, Subject];

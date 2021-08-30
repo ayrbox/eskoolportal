534dbc3347134a13d8489244ec17ca6facf31a55
ayrbox <sabin.dangol@hotmail.com>
2021-08-30 03:50:52 +0100

    WIP marks entiry with entity selector.

---
+++ src/components/EntitySelect.tsx
@@ -0,0 +1,43 @@
+import { ChangeEventHandler, useMemo } from 'react';
+import { Input } from 'reactstrap';
+
+export type FormSelectOption = {
+  label: string;
+  value: string;
+};
+
+export interface EntitySelectProps<T> {
+  name: string;
+  items: T[];
+  onSelect: (name: string, value: T) => void;
+  optionTransformer: (item: T) => FormSelectOption;
+}
+
+export type EntityType = {
+  id: string;
+};
+
+// export default function EventsForm<T>(rest: ListFormProps<T>)
+export default function EntitySelect<T extends EntityType>(
+  props: EntitySelectProps<T>
+) {
+  const { name, optionTransformer, onSelect, items } = props;
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    const item = items.find(({ id }) => id === e.target.value);
+    onSelect(name, item);
+  };
+  const options = useMemo(() => items.map(optionTransformer), items);
+
+  return (
+    <Input type="select" onChange={handleChange}>
+      <option></option>
+      {options.map(({ label, value }) => (
+        <option key={value} value={value}>
+          {label}
+        </option>
+      ))}
+    </Input>
+  );
+}

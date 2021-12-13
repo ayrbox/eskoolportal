c1cdba69c0fb9b73ff789c1462874732cba54174
ayrbox <sabin.dangol@hotmail.com>
2021-12-13 04:05:53 +0000

    Reinitialise migration.

--- prisma/migrations/20211203235757_class_order/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `order` to the `Class` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Class" ADD COLUMN     "order" INTEGER NOT NULL;

9c7978acba7d85521d3f349eb8790301cc90309c
ayrbox <sabin.dangol@hotmail.com>
2021-12-13 09:19:18 +0000

    Use UTC date for fiscalyear.

--- prisma/seed/fiscalYear.ts
+++ prisma/seed/fiscalYear.ts
@@ -3,23 +3,23 @@ import { PrismaClient } from "@prisma/client";
 const fiscalYears = [
   {
     name: "2077",
-    startDate: new Date(2020, 3, 14),
-    endDate: new Date(2021, 3, 13),
+    startDate: Date.UTC(2020, 3, 14),
+    endDate: Date.UTC(2021, 3, 13),
   },
   {
     name: "2078",
-    startDate: new Date(2021, 3, 14),
-    endDate: new Date(2022, 3, 13),
+    startDate: Date.UTC(2021, 3, 14),
+    endDate: Date.UTC(2022, 3, 13),
   },
   {
     name: "2079",
-    startDate: new Date(2022, 3, 14),
-    endDate: new Date(2023, 3, 13),
+    startDate: Date.UTC(2022, 3, 14),
+    endDate: Date.UTC(2023, 3, 13),
   },
   {
     name: "2080",
-    startDate: new Date(2023, 3, 14),
-    endDate: new Date(2024, 3, 13),
+    startDate: Date.UTC(2023, 3, 14),
+    endDate: Date.UTC(2024, 3, 13),
   },
 ];
 
@@ -27,8 +27,8 @@ export default async function seedFiscalYear(prisma: PrismaClient) {
   await prisma.fiscalYear.createMany({
     data: fiscalYears.map(({ name, startDate, endDate }) => ({
       name,
-      startDate,
-      endDate,
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
     })),
   });
 }

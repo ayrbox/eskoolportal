08bf076e1e5d748798aa9649e7429f502a7945e6
ayrbox <sabin.dangol@hotmail.com>
2021-12-13 09:43:21 +0000

    Subject.

--- src/pages/api/subjects/[subjectId]/index.ts
+++ src/pages/api/subjects/[subjectId]/index.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
-import { Subject } from "~/database/entities/Subject";
 import { secureRoute } from "~/lib/secureRoute";
+import prisma from "~/lib/prisma";
+import { Subject } from "@prisma/client";
 
 const handler = nextConnect();
 
@@ -9,12 +10,12 @@ handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const subjectId = req.query.subjectId as string;
     const data = req.body as Subject;
-    await Subject.update(
-      {
+    await prisma.subject.update({
+      data,
+      where: {
         id: subjectId,
       },
-      data
-    );
+    });
     res.status(200).send({ message: "Subject updated successfully." });
   } catch (err) {
     res.status(500).send({ message: "Unable to update subject." });

0fe3c6a49be375361d9ffc840d567f99382d05e0
ayrbox <sabin.dangol@hotmail.com>
2021-12-17 21:55:31 +0000

    Use prisma for events.

--- src/pages/api/events/[id].ts
+++ src/pages/api/events/[id].ts
@@ -1,25 +1,32 @@
-import nextConnect from 'next-connect';
-import { NextApiHandler } from 'next';
-import { secureRoute } from '~/lib/secureRoute';
-import { eventSchema } from '~/lib/validations';
-import { Event } from '~/database/entities/Event';
+import nextConnect from "next-connect";
+import { NextApiHandler } from "next";
+import { secureRoute } from "~/lib/secureRoute";
+import { eventSchema } from "~/lib/validations";
+import { Prisma } from "@prisma/client";
+import prisma from "~/lib/prisma";
 
 const updateHandler: NextApiHandler = async (req, res) => {
   const id = req.query.id as string;
-  const eventToUpdate = req.body as Event;
+  const eventToUpdate = req.body as Prisma.EventUpdateInput;
 
   await eventSchema.validate(eventToUpdate, { abortEarly: false });
-  const eventUpdated = await Event.update(id, eventToUpdate);
+  const eventUpdated = await prisma.event.update({
+    data: eventToUpdate,
+    where: {
+      id,
+    },
+  });
 
   res.send(eventUpdated);
 };
 
 const deleteHandler: NextApiHandler = async (req, res) => {
   const id = req.query.id as string;
-  (await Event.findOne(id)).softRemove();
+
+  await prisma.event.delete({ where: { id } });
 
   res.send({
-    message: 'Deleted',
+    message: "Deleted",
   });
 };
 
--- src/pages/api/events/index.ts
+++ src/pages/api/events/index.ts
@@ -1,22 +1,25 @@
 import { NextApiResponse, NextApiRequest } from "next";
 import nextConnect from "next-connect";
 import { secureRoute } from "~/lib/secureRoute";
-import { Event } from "~/database/entities/Event";
 import { eventSchema } from "~/lib/validations";
+import prisma from "~/lib/prisma";
+import { Prisma } from "@prisma/client";
 
 const handler = nextConnect();
 
 handler.get(async (_, res: NextApiResponse) => {
-  const events = await Event.find();
+  const events = await prisma.event.findMany();
   res.send(events);
 });
 
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
-  const eventData = req.body as Event;
+  const eventData = req.body as Prisma.EventCreateInput;
 
   await eventSchema.validate(eventData, { abortEarly: false });
 
-  const newEvent = await Event.create(eventData).save();
+  const newEvent = await prisma.event.create({
+    data: eventData,
+  });
   res.send(newEvent);
 });
 

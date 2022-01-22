e88fb4d8b2fe6794391a0856c23705fddbf2ab4f
ayrbox <sabin.dangol@hotmail.com>
2022-01-22 20:22:22 +0000

    Refactor fiscal seeder to make 2078 latest.

--- prisma/seed/fiscalYear.ts
+++ prisma/seed/fiscalYear.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const fiscalYears = [
   {
+    name: "2076",
+    startDate: Date.UTC(2019, 3, 14),
+    endDate: Date.UTC(2020, 3, 13),
+  },
+  {
     name: "2077",
     startDate: Date.UTC(2020, 3, 14),
     endDate: Date.UTC(2021, 3, 13),
@@ -10,16 +15,6 @@ const fiscalYears = [
     name: "2078",
     startDate: Date.UTC(2021, 3, 14),
     endDate: Date.UTC(2022, 3, 13),
-  },
-  {
-    name: "2079",
-    startDate: Date.UTC(2022, 3, 14),
-    endDate: Date.UTC(2023, 3, 13),
-  },
-  {
-    name: "2080",
-    startDate: Date.UTC(2023, 3, 14),
-    endDate: Date.UTC(2024, 3, 13),
   },
 ];
 
